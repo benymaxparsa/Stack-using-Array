20ba139aac8afb633a68e74d54b443c108167490
Parsa KamaliPour <parsakamalipour@gmail.com>
2020-01-24 01:18:32 +0330

    header is done

---
+++ Stack-using-Array/Stack.cpp
@@ -0,0 +1,30 @@
+#include "Stack.h"
+
+int Stack::pop()
+{
+	return 0;
+}
+
+void Stack::push(int x)
+{
+}
+
+int Stack::peek()
+{
+	return 0;
+}
+
+bool Stack::isEmpty()
+{
+	return false;
+}
+
+int Stack::size()
+{
+	return 0;
+}
+
+bool Stack::isFull()
+{
+	return false;
+}
